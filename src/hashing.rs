2b14e169bd39db9be6072799adde8695d15ba02a
Jør∂¡ <itsbrickpop@gmail.com>
2021-02-01 17:01:19 +0100

    Adding tests for compressed public keys

--- src/hashing.rs
+++ src/hashing.rs
@@ -44,7 +44,7 @@ mod tests {
     }
 
     #[test]
-    fn should_hash_hex_claims() {
+    fn should_hash_hex_pub_keys() {
         let hex_claim = "0x045a126cbbd3c66b6d542d40d91085e3f2b5db3bbc8cda0d59615deb08784e4f833e0bb082194790143c3d01cedb4a9663cb8c7bdaaad839cb794dd309213fcf30";
         let b64_hash = digest_hex_claim(hex_claim).unwrap();
         assert_eq!(b64_hash, "nGOYvS4aqqUVAT9YjWcUzA89DlHPWaooNpBTStOaHRA=");
@@ -88,6 +88,53 @@ mod tests {
         let hex_claim = "0x044c01f3d0ef3d60652aa7c6489b2f10edcae1b04a10460ab2d5e4bd752eb0686cac7aa6057fd4c65606e8a4c33c0b519b1764229395cde2c8537ee01136ef0776";
         let b64_hash = digest_hex_claim(hex_claim).unwrap();
         assert_eq!(b64_hash, "VS5c2JQT3x++ltSQHqnCFIBHttdjU2Lk2RuCGkUhnQ8=");
+    }
+
+    #[test]
+    fn should_hash_compressed_hex_pub_keys() {
+        let hex_claim = "0x025a126cbbd3c66b6d542d40d91085e3f2b5db3bbc8cda0d59615deb08784e4f83";
+        let b64_hash = digest_hex_claim(hex_claim).unwrap();
+        assert_eq!(b64_hash, "3cZFfsN4tUAhkBsnZ9AyGzK0Sg5PLKMvqRHclsxQcCY=");
+
+        let hex_claim = "0x029969c7741ade2e9f89f81d12080651038838e8089682158f3d892e57609b64e2";
+        let b64_hash = digest_hex_claim(hex_claim).unwrap();
+        assert_eq!(b64_hash, "pKH7yoPwmArs52+sAYP28F2Bbi4amtrVJoe4b6S9dRs=");
+
+        let hex_claim = "0x029622878da186a8a31f4dc03454dbbc62365060458db174618218b51d5014fa56";
+        let b64_hash = digest_hex_claim(hex_claim).unwrap();
+        assert_eq!(b64_hash, "RPNeqeFnKzLc6GKvi4iA0EmZRAlpp0BpTJkBfvPFmC8=");
+
+        let hex_claim = "0x03e355263aa6cbc99d2fdd0898f5ed8630115ad54e9073c41a8aa0df6d75842d8b";
+        let b64_hash = digest_hex_claim(hex_claim).unwrap();
+        assert_eq!(b64_hash, "Zk+WTQNF1+88t4qPjLqpO1qexc2fEeSEaetVn7B6OzA=");
+
+        let hex_claim = "0x02020d62c94296539224b885c6cdf79d0c2dd437471425be26bf62ab522949f83f";
+        let b64_hash = digest_hex_claim(hex_claim).unwrap();
+        assert_eq!(b64_hash, "wJ+U4/1yBMbmnwj8s0im9iQkVaR49EOuN/N2LFyFeBA=");
+
+        let hex_claim = "0x026bd65449f336b888fc36c64708940da0d1c864a0ac46236f60b455841a4d15c9";
+        let b64_hash = digest_hex_claim(hex_claim).unwrap();
+        assert_eq!(b64_hash, "r1Z6UwB6iX0l0Yx2FzOh8Nzv7VuTKqNF+4eYqcqaXjA=");
+
+        let hex_claim = "0x0212cf2bd4a9613ad988f7f008a5297b8e8c98df8759a2ef9d3dfae63b3870cfbb";
+        let b64_hash = digest_hex_claim(hex_claim).unwrap();
+        assert_eq!(b64_hash, "8YKaewMj9cIl/gPgITLbrUGYnLCo00pjTF2vFwI6CRc=");
+
+        let hex_claim = "0x02a2e6914db4a81ea9ec72e71b41cf88d4bc19ea54f29ae2beb3db8e4acf6531b5";
+        let b64_hash = digest_hex_claim(hex_claim).unwrap();
+        assert_eq!(b64_hash, "H+3VsMxmC5VsTSMawhh0kQ0Kt3EKQQl8ISsNp6TCHR8=");
+
+        let hex_claim = "0x031508189a6f1737f50dd2c603c1ded8a83f97073d33cbb317e7409c1487b8351a";
+        let b64_hash = digest_hex_claim(hex_claim).unwrap();
+        assert_eq!(b64_hash, "Pf/Q2nfiYnrUTS1Pabe1YjwdqLFX6Hc5M4KwZT9Nuxc=");
+
+        let hex_claim = "0x02f11597483032666b20ec51b27e1337577f63a5e1d5962575b555bf899380ae15";
+        let b64_hash = digest_hex_claim(hex_claim).unwrap();
+        assert_eq!(b64_hash, "D3uiR1w5Vu/7OPZdBZWxjq4FHsPF1vdU8uMsduUD0x0=");
+
+        let hex_claim = "0x024c01f3d0ef3d60652aa7c6489b2f10edcae1b04a10460ab2d5e4bd752eb0686c";
+        let b64_hash = digest_hex_claim(hex_claim).unwrap();
+        assert_eq!(b64_hash, "6fn61Hoz4P0v8STJSIUdz6GNi1NyyRBCj07qvQLToSw=");
     }
 
     #[test]
@@ -143,6 +190,65 @@ mod tests {
         let hex_claim = "0x04acdbbdba45841ddcc1c3cb2e8b696eae69ba9d57686bff0cd58e4033a08d9dc6c272a3577508cdb18bdb1c6fcc818538664bb6dc4cc32ee668198c7be044800c";
         let b64_hash = digest_hex_claim(hex_claim).unwrap();
         assert_eq!(b64_hash, "UPqwKZBMhq21uwgLWJUFMgCBMPzhseiziVaqN4EQvwA=");
+        let len = base64::decode(b64_hash)
+            .expect("The hash is not a valid base64")
+            .len();
+        assert_eq!(len, 32);
+    }
+
+    #[test]
+    fn should_return_32_byte_hashes_from_compressed() {
+        let hex_claim = "0x03c94699a259ec27e1cf67fe46653f0dc2f38e6d32abb33b45fc9ffe793171a44b";
+        let b64_hash = digest_hex_claim(hex_claim).unwrap();
+        assert_eq!(b64_hash, "fKYmOKLR9mG/VnmIg6I2aLe7ysowrTqkLLAfVazfHAs=");
+        let len = base64::decode(b64_hash)
+            .expect("The hash is not a valid base64")
+            .len();
+        assert_eq!(len, 32);
+
+        let hex_claim = "0x0324a71e7c24b38aaeeebbc334113045885bfae154071426e21c021ebc47a5a85a";
+        let b64_hash = digest_hex_claim(hex_claim).unwrap();
+        assert_eq!(b64_hash, "1unO9DU5kr7lbPUT9YyPY/EQcDr2KFyDJGPR3tTPSCs=");
+        let len = base64::decode(b64_hash)
+            .expect("The hash is not a valid base64")
+            .len();
+        assert_eq!(len, 32);
+
+        let hex_claim = "0x02ff51151c6bd759d723af2d0571df5e794c28b204242f4b540b0d3449eab192ca";
+        let b64_hash = digest_hex_claim(hex_claim).unwrap();
+        assert_eq!(b64_hash, "lV353gFkyjwoh8bjmzJ35ndkC2hof7o0+Tw2x9VPYQs=");
+        let len = base64::decode(b64_hash)
+            .expect("The hash is not a valid base64")
+            .len();
+        assert_eq!(len, 32);
+
+        let hex_claim = "0x023f10ff1b295bf4d2f24c40c93cce04210ae812dd5ad1a06d5dafd9a2e18fa124";
+        let b64_hash = digest_hex_claim(hex_claim).unwrap();
+        assert_eq!(b64_hash, "GnkH1v5cCoLRddAJL13rA5BCyaixVzD8Su2x5FEW4iE=");
+        let len = base64::decode(b64_hash)
+            .expect("The hash is not a valid base64")
+            .len();
+        assert_eq!(len, 32);
+
+        let hex_claim = "0x0309d240a33ca9c486c090135f06c5d801aceec6eaed94b8bef1c9763b6c397088";
+        let b64_hash = digest_hex_claim(hex_claim).unwrap();
+        assert_eq!(b64_hash, "DuOZ467hKLeRjJbhuZy/h4s6FnjLRXAJDSPElEYTOwo=");
+        let len = base64::decode(b64_hash)
+            .expect("The hash is not a valid base64")
+            .len();
+        assert_eq!(len, 32);
+
+        let hex_claim = "0x02220da30ddd87fed1b65ef75706507f397138d8cac8917e118157124b7e1cf45b";
+        let b64_hash = digest_hex_claim(hex_claim).unwrap();
+        assert_eq!(b64_hash, "9pAfgqDFOTkUgYmIRDiopFWNgXIb069EPaInKgGLKRA=");
+        let len = base64::decode(b64_hash)
+            .expect("The hash is not a valid base64")
+            .len();
+        assert_eq!(len, 32);
+
+        let hex_claim = "0x02acdbbdba45841ddcc1c3cb2e8b696eae69ba9d57686bff0cd58e4033a08d9dc6";
+        let b64_hash = digest_hex_claim(hex_claim).unwrap();
+        assert_eq!(b64_hash, "oWlFPwaVT762OHFYdgcGbvkDgSwbMqf5DiqdLwlzohE=");
         let len = base64::decode(b64_hash)
             .expect("The hash is not a valid base64")
             .len();
